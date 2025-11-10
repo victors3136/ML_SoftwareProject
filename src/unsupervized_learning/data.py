933a6a8fe60a845e224a5f0c7e2dcf455cd8507c
victors3136 <victor.selegean@gmail.com>
2025-11-10 18:30:12 +0200

    Begin implementation of Clustering Node

--- src/unsupervized_learning/data.py
+++ src/unsupervized_learning/data.py
@@ -1,5 +1,6 @@
-from typing import LiteralString, List, Dict
+from typing import LiteralString, List
 
+from lib.framework_types import Number, NumberType
 from src.lib.data import load_data
 from src.lib.features import BaseFeatures
 from src.lib.pipeline import make_pipeline, pipeline_apply
@@ -32,24 +33,18 @@ _pipeline = make_pipeline(
 )
 
 
-class Cluster:
+class Node:
     def __init__(self, frame: Frame):
-        self._data: Frame = pipeline_apply(frame, _pipeline)
+        frame = pipeline_apply(frame, _pipeline)
+        assert frame.shape[0] == 1, f"A node must represent a single row, not {frame.shape[0]}"
+        row_data: dict[str, Number] = frame.get_row(0)
+        assert all(isinstance(value, NumberType) for value in row_data.values())
+        self.data = row_data
 
-    def centroid(self) -> Dict[str, float]:
-        return self._data.column_means()
-
-    def head(self):
-        return self._data.head()
-
-    @property
-    def data(self) -> Frame:
-        return self._data
+    def __sub__(self, other: Node):
+        ...
 
 
 if __name__ == "__main__":
     dataset: Frame = load_data()
     print(dataset)
-
-    data = Cluster(dataset)
-    print(data.data)
